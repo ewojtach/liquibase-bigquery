3fe670beb716c0929346bdbd83db5127d57a608a
Ewa Wojtach <ewojtach@google.com>
2022-09-01 09:42:39 +0200

    dataset creation

--- src/test/resources/terraform/main.tf
+++ src/test/resources/terraform/main.tf
@@ -4,9 +4,8 @@ locals {
 
 module "bigquery" {
   source            = "terraform-google-modules/bigquery/google" # Path to the module
-  version           = "~> 2.0.0" # Specify the version of the module you require
-  dataset_id        = "harness-test"
-  dataset_name      = "harness-test"
+  dataset_id        = "harness_test_ds"
+  dataset_name      = "harness_test_ds"
   description       = "DataSet for liquibase harness tests to be run" # updated the description accordingly
   project_id        = var.project_id
   location          = "US" # Update location if needed
