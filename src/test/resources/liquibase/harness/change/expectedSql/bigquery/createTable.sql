5b558b9d088f0aff4620a6b6f70080b324b64e96
Łukasz Olejniczak <lolejniczak@google.com>
2022-09-02 20:22:32 +0200

    fix

--- src/test/resources/liquibase/harness/change/expectedSql/bigquery/createTable.sql
+++ src/test/resources/liquibase/harness/change/expectedSql/bigquery/createTable.sql
@@ -1,1 +1,1 @@
-CREATE TABLE test_table (test_id INT, test_column STRING)
+CREATE TABLE lharness.test_table (test_id INT, test_column STRING)
---
+++ src/test/resources/liquibase/harness/change/expectedSql/bigquery/dropView.sql
@@ -0,0 +1,2 @@
+CREATE VIEW lharness.test_view AS select id, first_name, last_name, email from authors
+DROP VIEW lharness.test_view
