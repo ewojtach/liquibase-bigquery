73180ca459b088bc8cfcf6720ba6492602c2b631
Ewa Wojtach <ewojtach@google.com>
2022-09-09 17:18:52 +0200

    cleanup

--- src/test/resources/terraform/main.tf
+++ src/test/resources/terraform/main.tf
@@ -3,13 +3,14 @@ locals {
 }
 
 module "bigquery" {
-  source            = "terraform-google-modules/bigquery/google" # Path to the module
-  version           = "~> 2.0.0"
-  dataset_id        = "harness_test_ds"
-  dataset_name      = "harness_test_ds"
-  description       = "DataSet for liquibase harness tests to be run" # updated the description accordingly
-  project_id        = var.project_id
-  location          = "US" # Update location if needed
-  dataset_labels    = {}
-  time_partitioning = "DAY"
+  source                     = "terraform-google-modules/bigquery/google" # Path to the module
+  version                    = "~> 2.0.0"
+  dataset_id                 = "harness_test_ds"
+  dataset_name               = "harness_test_ds"
+  description                = "DataSet for liquibase harness tests to be run" # updated the description accordingly
+  project_id                 = var.project_id
+  location                   = "US" # Update location if needed
+  delete_contents_on_destroy = true
+  dataset_labels             = {}
+  time_partitioning          = "DAY"
 }
