504a0be0ca37d3bdc0f63e2454650e990b1617dc
Ewa Wojtach <ewojtach@google.com>
2022-09-09 18:13:23 +0200

    outputs

--- src/test/resources/terraform/outputs.tf
+++ src/test/resources/terraform/outputs.tf
@@ -1,9 +1,1 @@
-output "dataset_id" {
-  value       = module.bigquery.dataset_id
-  description = "Unique id for the dataset being provisioned"
-}
 
-output "dataset_name" {
-  value       = module.bigquery.dataset_name
-  description = "Friendly name for the dataset being provisioned"
-}
