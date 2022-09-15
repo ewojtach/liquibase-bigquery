238214ef7e06ce97752580ce289f1eaf65e41007
lolejniczak <bisideu@gmail.com>
2022-09-15 09:00:54 +0200

    invalidate trigger tests

--- src/test/resources/liquibase/harness/change/expectedSql/bigquery/addColumn.sql
+++ src/test/resources/liquibase/harness/change/expectedSql/bigquery/addColumn.sql
@@ -1,3 +1,8 @@
-ALTER TABLE authors ADD COLUMN varcharColumn STRING
+INVALID TEST
+'ALTER TABLE authors ADD COLUMN varcharColumn STRING
 ALTER TABLE authors ADD COLUMN intColumn INT64
 ALTER TABLE authors ADD COLUMN dateColumn date
+UPDATE authors SET varcharColumn = "INITIAL_VALUE"" WHERE 1=1
+UPDATE authors SET intColumn = 5 WHERE 1=1
+UPDATE authors SET dateColumn = "2020-09-21" WHERE 1=1
+'
